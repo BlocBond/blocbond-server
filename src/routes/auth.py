a17b78902755ebcc3d399470bca036da87e57b13
ericbuys <eric.buys02@gmail.com>
2024-05-05 01:47:06 -0400

    updating user db

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -12,43 +12,44 @@ users = {}
 def authenticate():
     data = json.loads(request.data.decode('utf-8'))
 
-    # data = request.json
     username = data.get('username')
     password = data.get('password')
-    print(1)
+    print(username)
     if not username or not password:
         return jsonify({'error': 'Username and password are required'}), 400
-    print(2)
     token = create_access_token(identity=username, expires_delta=False)
     if username not in users:
-        print(3)
+        print('New default account created')
+
         # Hash the password before storing it
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
         # Store the user in the dictionary (in the future we will use database)
         users[username] = {'username': username, 'password': hashed_password, 'google': False}  
-        print(4)
-        response = jsonify({'message': 'Success', 'token': token})
-        # response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+
+        return jsonify({'message': 'Success', 'token': token}), 201
     else:
-        print(5)
-        if bcrypt.checkpw(password.encode('utf-8'), users[username][password]):
-            print(6)
+        if bcrypt.checkpw(password.encode('utf-8'), users[username]["password"].encode('utf-8')):
+            print('Signed in with existing default account')
+
             return jsonify({'message': 'Success', 'token': token}), 200
-    print(7)
+        
     return jsonify({'message': 'Unable to authenticate', 'token': None}), 403
 
 @app.route('/google_authenticated', methods=['POST'])
 def authenticated_user_data():
-    data = request.json
+    data = json.loads(request.data.decode('utf-8'))
+
     username = data.get('username')
     token = create_access_token(identity=username, expires_delta=False)
-
+    print(username)
     if username not in users:
+        print('New google account created')
+
         users[username] = {'username': username, 'password': "google-protected", 'google': True}
         return jsonify({'message': 'Success', 'token': token}), 201
-    
+
+    print('Signed in with existing google account')
     return jsonify({'message': 'Success', 'token': token}), 200
 
 @app.route("/logout", methods=["POST"])
@@ -57,6 +58,6 @@ def logout():
     data = request.json
     username = data.get('username')
 
-    del users[username]
+    # del users[username]
     return {"message": "Logged out successfully"}
 
